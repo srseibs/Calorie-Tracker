fd2f8a23fca5ad793d4d3deb0df219cc54bde690
Mike Seibel <srseibs@gmail.com>
2022-01-29 18:05:43 -0800

    Finished Activity and Goal screens.

--- app/src/main/java/com/plcoding/calorytracker/MainActivity.kt
+++ app/src/main/java/com/plcoding/calorytracker/MainActivity.kt
@@ -14,8 +14,10 @@ import androidx.navigation.compose.composable
 
 import androidx.navigation.compose.rememberNavController
 import com.example.core.navigation.Route
+import com.example.onboarding_presentation.activity.ActivityScreen
 import com.example.onboarding_presentation.age.AgeScreen
 import com.example.onboarding_presentation.gender.GenderScreen
+import com.example.onboarding_presentation.goal.GoalScreen
 import com.example.onboarding_presentation.height.HeightScreen
 import com.example.onboarding_presentation.weight.WeightScreen
 import com.example.onboarding_presentation.welcome.WelcomeScreen
@@ -63,15 +65,22 @@ class MainActivity : ComponentActivity() {
                                 onNavigate = navController::navigate
                             )
                         }
+                        composable(Route.ACTIVITY) {
+                            ActivityScreen(
+                                onNavigate = navController::navigate
+                            )
+                        }
+                        composable(Route.GOAL) {
+                            GoalScreen(
+                                onNavigate = navController::navigate
+                            )
+                        }
+
                         composable(Route.NUTRIENT_GOAL) {
 
                         }
-                        composable(Route.ACTIVITY) {
 
-                        }
-                        composable(Route.GOAL) {
 
-                        }
                         composable(Route.TRACKER_OVERVIEW) {
 
                         }
---
+++ onboarding/onboarding_presentation/src/main/java/com/example/onboarding_presentation/activity/ActivityScreen.kt
@@ -0,0 +1,106 @@
+package com.example.onboarding_presentation.activity
+
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.core.domain.model.ActivityLevel
+import com.example.core.domain.model.Gender
+import com.example.core.util.UiEvent
+import com.example.core_ui.LocalSpacing
+import com.example.onboarding_presentation.components.ActionButton
+import com.example.onboarding_presentation.components.SelectableButton
+import com.plcoding.core.R
+
+import kotlinx.coroutines.flow.collect
+
+
+@Composable
+fun ActivityScreen(
+    onNavigate: (UiEvent.Navigate) -> Unit,
+    viewModel: ActivityViewModel = hiltViewModel()
+) {
+
+    val spacing = LocalSpacing.current
+    LaunchedEffect(key1 = true) {
+        viewModel.uiEvent.collect { event ->
+            when (event) {
+                is UiEvent.Navigate -> onNavigate(event)
+                else -> Unit
+            }
+        }
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(spacing.spaceLarge)
+    ){
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Text(
+                text = stringResource(id = R.string.whats_your_activity_level),
+                style = MaterialTheme.typography.h3
+            )
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
+            Row{
+                SelectableButton(
+                    text = stringResource(id = R.string.low),
+                    isSelected = viewModel.selectedActivityLevel is ActivityLevel.Low,
+                    color = MaterialTheme.colors.primaryVariant,
+                    selectedTextColor = MaterialTheme.colors.onPrimary,
+                    onClick = {
+                        viewModel.onActivityLevelSelect(ActivityLevel.Low)
+                    },
+                    textStyle = MaterialTheme.typography.button.copy(
+                        fontWeight = FontWeight.Normal
+                    )
+                )
+                Spacer(modifier = Modifier.width(spacing.spaceMedium))
+                SelectableButton(
+                    text = stringResource(id = R.string.medium),
+                    isSelected = viewModel.selectedActivityLevel is ActivityLevel.Medium,
+                    color = MaterialTheme.colors.primaryVariant,
+                    selectedTextColor = MaterialTheme.colors.onPrimary,
+                    onClick = {
+                        viewModel.onActivityLevelSelect(ActivityLevel.Medium)
+                    },
+                    textStyle = MaterialTheme.typography.button.copy(
+                        fontWeight = FontWeight.Normal
+                    )
+                )
+                Spacer(modifier = Modifier.width(spacing.spaceMedium))
+                SelectableButton(
+                    text = stringResource(id = R.string.high),
+                    isSelected = viewModel.selectedActivityLevel is ActivityLevel.High,
+                    color = MaterialTheme.colors.primaryVariant,
+                    selectedTextColor = MaterialTheme.colors.onPrimary,
+                    onClick = {
+                        viewModel.onActivityLevelSelect(ActivityLevel.High)
+                    },
+                    textStyle = MaterialTheme.typography.button.copy(
+                        fontWeight = FontWeight.Normal
+                    )
+                )
+                Spacer(modifier = Modifier.width(spacing.spaceMedium))
+            }
+        }
+
+        ActionButton(
+            text = stringResource(R.string.next),
+            onClick = viewModel::onNextClick,
+            modifier = Modifier.align(Alignment.BottomEnd)
+        )
+
+    }
+}
