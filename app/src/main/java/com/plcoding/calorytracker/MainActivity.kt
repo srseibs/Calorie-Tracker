2981f1d3950b20e98f9db7cf32d09a8f463f93c7
Mike Seibel <srseibs@gmail.com>
2022-01-30 13:44:58 -0800

    Finished Nutrient Goal screen.

--- app/src/main/java/com/plcoding/calorytracker/MainActivity.kt
+++ app/src/main/java/com/plcoding/calorytracker/MainActivity.kt
@@ -19,6 +19,7 @@ import com.example.onboarding_presentation.age.AgeScreen
 import com.example.onboarding_presentation.gender.GenderScreen
 import com.example.onboarding_presentation.goal.GoalScreen
 import com.example.onboarding_presentation.height.HeightScreen
+import com.example.onboarding_presentation.nutrient_goal.NutrientGoalScreen
 import com.example.onboarding_presentation.weight.WeightScreen
 import com.example.onboarding_presentation.welcome.WelcomeScreen
 import com.plcoding.calorytracker.navigation.navigate
@@ -77,15 +78,18 @@ class MainActivity : ComponentActivity() {
                         }
 
                         composable(Route.NUTRIENT_GOAL) {
-
+                            NutrientGoalScreen(
+                                scaffoldState = scaffoldState,
+                                onNavigate = navController::navigate
+                            )
                         }
 
 
                         composable(Route.TRACKER_OVERVIEW) {
-
+                            Text("Tracker overview screen")
                         }
                         composable(Route.SEARCH) {
-
+                            Text("Search screen")
                         }
                     }
                 }
---
+++ onboarding/onboarding_domain/src/main/java/com/example/onboarding_domain/di/OnboardingDomainModule.kt
@@ -0,0 +1,20 @@
+package com.example.onboarding_domain.di
+
+import com.example.onboarding_domain.use_case.ValidateNutrients
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ViewModelComponent::class)
+object OnboardingDomainModule {
+
+    @Provides
+    @ViewModelScoped
+    fun provideValidateNutrientsUseCase(): ValidateNutrients {
+        return ValidateNutrients()
+    }
+}
